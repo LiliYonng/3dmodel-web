5fb9e87cf792bc4ebf7e5186bc58207002a8968b
LiliYonng <lili.yonng@gmail.com>
2022-09-01 16:13:13 +0800

    删除多余文件

--- .history/src/api/mock_20220831221322.js
+++
@@ -1,4 +0,0 @@
-import Mock from 'mockjs'
-import modelApi from './mockData/modelData.js'
-Mock.mock('/api/model/getModel',modelApi.returnData )
-//Mock.mock(/\/api\/model\/getModel/, 'post',modelApi.returnData)
--- .history/src/api/mock_20220831233007.js
+++
@@ -1,5 +0,0 @@
-import Mock from 'mockjs'
-import modelApi from './mockData/modelData.js'
-Mock.mock('/api/model/getModel',modelApi.returnData )
-//Mock.mock(/\/api\/model\/getModel/, 'post',modelApi.returnData)
-Mock.mock(/\/api\/login\/getToken/, 'get',modelApi.returnData)
