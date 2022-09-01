4b23b4245d5510a7aabd5393bb16bc5de99727ad
LiliYonng <lili.yonng@gmail.com>
2022-09-01 09:48:13 +0800

    '接口和数据封装'

---
+++ .history/src/api/mockData/modelData_20220831234049.js
@@ -0,0 +1,12 @@
+ 
+export default {
+    returnData:(config)=>{
+        const id =config.body;
+        return{
+            code:200,
+            data:{
+                a:'aaa',
+            }
+        }
+    }
+}
---
+++ .history/src/store/modelData_20220901010326.js
@@ -0,0 +1,19 @@
+import Cookies from "js-cookie";
+export default {
+    state:{
+        modelAry:[]
+    },
+    mutations:{
+        setModelAry(state,val){
+            Cookies.set('modelAry',val);
+            state.modelAry = val;
+        },
+        getModelAry(state){
+            state.modelAry = state.modelAry || Cookies.get('modelAry');
+        },
+        clearModelAry(state){
+            state.modelAry=[];
+            Cookies.remove("modelAry");
+        }
+    }
+}
