2741ab2165f949b86658df0b1b91123e2f43717e
LiliYonng <[lili.yonng@gmail.com]>
2022-10-01 21:30:48 +0800

    修改存取cookie位置，添加过期时间

--- src/api/mockData/modelData.js
+++ src/api/mockData/modelData.js
@@ -1,14 +1,5 @@
 
-const modelAry = [
-      {
-        groupId:1004,
-        name: "model1",
-        type: "obj",
-        texAry: ["color", "specular", "gilt"],
-        path: "/model/model1/",
-        scale:[0.4,0.4,0.4],
-        position:[0,5,-8]
-      },    
+const modelAry = [      
       {
         groupId:1004,
         name: "model2",
@@ -19,6 +10,15 @@ const modelAry = [
         position:[0,-40,0]
       },
       {
+        groupId:1004,
+        name: "model1",
+        type: "obj",
+        texAry: ["color", "specular", "gilt"],
+        path: "/model/model1/",
+        scale:[0.4,0.4,0.4],
+        position:[0,5,-8]
+      },    
+      {
         groupId:1001,
         name: "model3",
         type: "gltf",
@@ -27,6 +27,15 @@ const modelAry = [
         scale:[60,60,60],
         position:[0,-10,0]
       },  
+      {
+        groupId:1001,
+        name: "model2",
+        type: "gltf",
+        texAry: ["color", "specular", "gilt"],
+        path: "/model/model2/",
+        scale:[2,2,2],
+        position:[0,-40,0]
+      },
 ]
 
 export default {
--- src/main.js
+++ src/main.js
@@ -9,15 +9,6 @@ import 'vant/lib/index.css'
 Vue.use(Vant);
 
 Vue.config.productionTip = false
-router.beforeEach((to,from,next)=>{
-  store.commit('getModelAry')
-  const ary = store.state.modelData.modelAry;
-  if(ary.length===0 && to.name==='model'){
-    next({name:'home'});
-  }else{
-    next();
-  }
-})
 new Vue({
   router,
   store,
